3e6ed52eaa9a4979a73408877834d108160678e5
Mohamed Ayman <mohdayman22rr@gmail.com>
2023-05-26 23:06:43 +0300

    ADD: super

---
+++ super.js
@@ -0,0 +1,34 @@
+// Tree class
+class Tree {
+  constructor(species) {
+    this.species = species;
+  }
+
+  static definition() {
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
+  }
+}
+
+// Deciduous class (extends Tree)
+class Deciduous extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return `${super.definition()} Deciduous trees shed their leaves annually.`;
+  }
+}
+
+// Evergreen class (extends Tree)
+class Evergreen extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return `${super.definition()} Evergreens keep their leaves all year round.`;
+  }
+}
